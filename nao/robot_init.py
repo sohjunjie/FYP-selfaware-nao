8c99e2a547d5b4f46efd9180bedd4d793683d7d1
sohjunjie <junjie.soh93@gmail.com>
2018-10-05 15:24:43 +0800

    enable face detection and dialog speech recognition

--- nao/robot_init.py
+++ nao/robot_init.py
@@ -8,7 +8,7 @@ import config as cf
 
 
 # Global variable to store the stimuliEventWatcher module instance
-stimuliEventWatcher = None
+global stimuliEventWatcher
 
 
 class WorldStimuliEventWatcher(ALModule):
@@ -17,8 +17,18 @@ class WorldStimuliEventWatcher(ALModule):
     def __init__(self):
         ALModule.__init__(self, "stimuliEventWatcher")
 
+        self.asr = ALProxy("ALSpeechRecognition", cf.ROBOT_IP, cf.ROBOT_PORT)
+        self.asr.setLanguage("English")
+        # self.asr.pause(True)
+        # with open('dictionary.txt') as f:
+        #     self.asr.setVocabulary(['hello', 'robot', 'cool'], False)
+        # self.asr.pause(False)
+
         self.dialog = ALProxy('ALDialog', cf.ROBOT_IP, cf.ROBOT_PORT)
         self.dialog.setLanguage("English")
+        self.dialog_topic = self.dialog.loadTopic('/var/persistent/home/nao/HumanDialog/HumanDialog_enu.top')
+        self.dialog.subscribe('myModule')
+        self.dialog.activateTopic(self.dialog_topic)
 
         self.memory = ALProxy("ALMemory", cf.ROBOT_IP, cf.ROBOT_PORT)
         self.memory.subscribeToEvent("ALBasicAwareness/HumanTracked",
@@ -38,6 +48,8 @@ class WorldStimuliEventWatcher(ALModule):
         self.is_sound_detection_started = True
         self.is_human_tracked = False
         self.is_dialog_detection_started = False
+        self.got_face = False
+        self.human_tracked = None
 
     def say(self, msg):
         self.tts.say(msg)
@@ -46,30 +58,35 @@ class WorldStimuliEventWatcher(ALModule):
         """ callback for event HumanTracked """
         self.is_human_tracked = True
         self.stop_sound_detection()
-        logging.info("got HumanTracked: detected person with ID:" + str(value))
-        self.say("Hello there.")
-        if value >= 0:  # found a new person
-            position_human = self.get_people_perception_data(value)
-            expression_human = self.get_people_expression_data(value)
-            [x, y, z] = position_human
-            print "The tracked person with ID", value, "is at the position:", \
-                "x=", x, "/ y=",  y, "/ z=", z
+        self.start_dialog_detection()
+        if value >= 0:
+            logging.info("got HumanTracked: detected person with ID:" + str(value))
 
     def onHumanLost(self, key, value, msg):
         """ callback for event HumanLost """
         self.is_human_tracked = False
+        self.human_tracked = None
         logging.info("got HumanLost: lost human" + str(value))
-        self.stop_sound_detection()
+        self.stop_dialog_detection()
         self.start_sound_detection()
 
     def onFaceDetected(self, key, value, msg):
-        print "key:", str(key), " value:", str(value)
-        self.tts.say('hello stranger')
-        self.start_sound_detection()
+        if value == []:  # empty value when the face disappears
+            self.got_face = False
+            self.human_tracked = None
+        elif not self.got_face:  # only speak the first time a face appears
+            self.got_face = True
+            self.human_tracked = value[1][0][1][2] if value[1][0][1][2] != '' else 'stranger'
+            self.say("I see you, " + self.human_tracked + ".")
 
     def onDialogDetected(self, key, value, msg):
-        self.tts.say(value)
-        logging.info("heard:" + value)
+        if not value:
+            return
+        self.stop_dialog_detection()
+        if self.got_face and self.human_tracked:
+            self.say(self.human_tracked + ", you said " + value)
+            logging.info("heard: " + value)
+        self.start_dialog_detection()
 
     def onSoundLocated(self, key, value, msg):
         self.tts.say("I heard something.")
@@ -101,26 +118,19 @@ class WorldStimuliEventWatcher(ALModule):
             self.is_dialog_detection_started = True
 
     def stop_dialog_detection(self):
-        if self.is_dialog_detection_started::
+        if self.is_dialog_detection_started:
             self.memory.unsubscribeToEvent("Dialog/LastInput", "stimuliEventWatcher")
             self.is_dialog_detection_started = False
 
-    def get_people_perception_data(self, id_person_tracked):
-        self.memory = ALProxy("ALMemory", cf.ROBOT_IP, cf.ROBOT_PORT)
-        memory_key = "PeoplePerception/Person/" + str(id_person_tracked) + \
-                     "/PositionInWorldFrame"
-        return self.memory.getData(memory_key)
-
-    def get_people_expression_data(self, id_person_tracked):
-        self.memory = ALProxy("ALMemory", cf.ROBOT_IP, cf.ROBOT_PORT)
-        memory_key = "PeoplePerception/Person/" + str(id_person_tracked) + \
-                     "/ExpressionProperties"
-        return self.memory.getData(memory_key)
+    def on_stop(self):
+        self.dialog.unsubscribe('myModule')
+        self.dialog.deactivateTopic(self.dialog_topic)
+        self.dialog.unloadTopic(self.dialog_topic)
 
 
 if __name__ == "__main__":
     cf.init()
-    event_broker = ALBroker("event_broker", "0.0.0.0", 0, cf.cf.ROBOT_IP, cf.cf.ROBOT_PORT)
+    event_broker = ALBroker("event_broker", "0.0.0.0", 0, cf.ROBOT_IP, cf.ROBOT_PORT)
     global stimuliEventWatcher
     stimuliEventWatcher = WorldStimuliEventWatcher()
     basic_awareness = ALProxy("ALBasicAwareness", cf.ROBOT_IP, cf.ROBOT_PORT)
@@ -137,7 +147,7 @@ if __name__ == "__main__":
             time.sleep(1)
     except KeyboardInterrupt:
         print "Interrupted by user, shutting down"
-        #stop
+        stimuliEventWatcher.on_stop()
         basic_awareness.stopAwareness()
         motion.rest()
         event_broker.shutdown()
