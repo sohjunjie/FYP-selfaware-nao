90213676efd301744e945640f41d2294f9093074
sohjunjie <junjie.soh93@gmail.com>
2019-01-16 19:52:11 +0800

    add methods to query from memory about human, robot, and registering of new info about human

--- pc/memory.py
+++ pc/memory.py
@@ -1,7 +1,8 @@
 from database.definition import Experiential, Mental, Social, Identity, \
     SocalProperties, IdentityProperties, upsert
-from pony.orm import db_session, commit
-
+from pony.orm import db_session, commit, select
+from datetime import datetime as dt
+from datetime import timedelta as td
 
 class Memory():
 
@@ -11,6 +12,15 @@ class Memory():
 
     @db_session
     def save_experiential(self, robot_exp):
+        if self.awareness.exeProc.dialogueManager.context['conversation_id'] is None:
+            conv_id = 0
+            try:
+                conv_id = select(exp.conversation_id for exp in Experiential).max() + 1
+            except:
+                pass
+            self.awareness.exeProc.dialogueManager.context['conversation_id'] = conv_id
+
+        context_conv_id = self.awareness.exeProc.dialogueManager.context['conversation_id']
         exp = Experiential(subject=robot_exp['subject'],
                            target=robot_exp['target'],
                            physicalAct=robot_exp['physicalAct'],
@@ -26,11 +36,12 @@ class Memory():
                            emotional_tentative=robot_exp['emotionalState'][6],
                            place_lat=robot_exp['place'][0],
                            place_lng=robot_exp['place'][1],
-                           exp_datetime=robot_exp['datetime'])
+                           exp_datetime=robot_exp['datetime'],
+                           conversation_id=context_conv_id)
         commit()
 
     @db_session
-    def update_social_prop(self, props):
+    def update_social_prop(self, person_name, props):
         """
         props = {
             'person': Social(name='junjie'),
@@ -39,5 +50,97 @@ class Memory():
             'property_value_int': 25
         }
         """
-        upsert(SocalProperties, props)
+
+        persons = select(p for p in Social if
+                    p.name == person_name
+                 )[:1]
+        
+        if len(persons) == 0:
+            return False
+
+        person = persons[0]
+        props['person'] = person
+        sp = upsert(SocalProperties, props)
         commit()
+
+        return sp
+
+    @db_session
+    def have_met_before(self, human):
+        """
+        Determine if robot has met human before by querying
+        the experiential aspect
+        """
+
+        context_conv_id = self.awareness.exeProc.dialogueManager.context['conversation_id']
+        if context_conv_id is None:
+            return False
+
+        q = select(e for e in Experiential if
+                e.conversation_id < context_conv_id
+            )[:1]
+        return len(q) > 0
+
+    @db_session
+    def get_robot_name(self):
+        q = select(i for i in Identity)[:1]
+        return q[0].name
+
+    @db_session
+    def query_robot(self, prop_name):
+
+        idProps = select(ip for ip in IdentityProperties if 
+                    ip.property_name == prop_name
+                  )[:1]
+
+        if len(idProps) == 0:
+            return ''
+        idProp = idProps[0]
+
+        if idProp.property_type == 'str':
+            return idProp.property_value_str
+        if idProp.property_type == 'int':
+            return str(idProp.property_value_int)
+        if idProp.property_type == 'float':
+            return str(idProp.property_value_float)
+        if idProp.property_type == 'datetime':
+            return str(idProp.property_value_datetime)
+        if idProp.property_type == 'decimal':
+            return str(idProp.property_value_decimal)
+        if idProp.property_type == 'bool':
+            return str(idProp.property_value_bool)
+
+        return ''
+
+
+    @db_session
+    def query_human(self, human_name, prop_name):
+        
+        socialHuman = select(s for s in Social if s.name == human_name)[:1]
+
+        if len(socialHuman) == 0:
+            return ''
+
+        socialProps = select(sp for sp in SocalProperties if 
+                    sp.person == socialHuman[0] and
+                    sp.property_name == prop_name
+                  )[:1]
+
+        if len(socialProps) == 0:
+            return ''
+
+        socialProp = socialProps[0]
+        if socialProp.property_type == 'str':
+            return socialProp.property_value_str
+        if socialProp.property_type == 'int':
+            return str(socialProp.property_value_int)
+        if socialProp.property_type == 'float':
+            return str(socialProp.property_value_float)
+        if socialProp.property_type == 'datetime':
+            return str(socialProp.property_value_datetime)
+        if socialProp.property_type == 'decimal':
+            return str(socialProp.property_value_decimal)
+        if socialProp.property_type == 'bool':
+            return str(socialProp.property_value_bool)
+
+        return ''
